3022f9c8493a6bc84e11181455889c147e9b9bbf
nishants <nishant.singh87@gmail.com>
2019-01-27 19:24:53 +0530

    Show resources.

--- src/components/user_wallet/index.js
+++ src/components/user_wallet/index.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { Statistic, Row, Col, Button } from 'antd';
+import {Statistic, Row, Col, Button, Progress} from 'antd';
 import {toEOSString} from '../../utils';
 
-const LABEL_STYLE = {marginBottom: "20px"};
+const LABEL_STYLE = {marginBottom: "50px"};
 const UserWallet = ({wallet}) => {
-    return <Row>
+    const
+        resources = wallet.resource,
+        ramUsage = parseInt(100 * resources.ram.available / resources.ram.total),
+        netUsage = parseInt(100 * resources.net.available / resources.net.total),
+        cpuUsage = parseInt(100 * resources.cpu.available / resources.cpu.total);
+
+    return <>
+        <Row>
         <Col span={8} style={LABEL_STYLE}>
-            <Statistic title="Total worth" value={toEOSString(wallet.balance.totalWorth)} />
+            <Statistic title="Total worth" value={wallet.balance.totalWorth} suffix="EOS"/>
         </Col>
         <Col span={8} style={LABEL_STYLE}>
-            <Statistic title="Liquid tokens" value={toEOSString(wallet.balance.liquidToken)} />
+            <Statistic title="Liquid tokens" value={wallet.balance.liquidToken} suffix="EOS"/>
         </Col>
         <Col span={8} style={LABEL_STYLE}>
-            <Statistic title="Net staked" value={toEOSString(wallet.balance.netStaked)} />
+            <Statistic title="Net staked" value={wallet.balance.netStaked} suffix="EOS"/>
         </Col>
         <Col span={8} style={LABEL_STYLE}>
-            <Statistic title="CPU staked" value={toEOSString(wallet.balance.cpuStaked)} />
+            <Statistic title="CPU staked" value={wallet.balance.cpuStaked} suffix="EOS"/>
         </Col>
     </Row>
+        <Row>
+            <Col span={8} style={LABEL_STYLE}>
+                <Progress type="circle" percent={ramUsage} format={ value => `RAM ${value}%`} />
+            </Col>
+            <Col span={8} style={LABEL_STYLE}>
+                <Progress type="circle" percent={cpuUsage} format={ value => `CPU ${value}%`} />
+            </Col>
+            <Col span={8} style={LABEL_STYLE}>
+                <Progress type="circle" percent={netUsage} format={ value => `NET ${value}%`} />
+            </Col>
+        </Row>
+        </>
 };
 
 export default UserWallet;
